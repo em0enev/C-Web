27afd770af6af853c4ddb666333b96eeeab80b9b
Emil Enev <emil.enev@abv.bg>
2019-05-19 11:56:23 +0300

    basic chronometer

---
+++ Asynchronous processing/Chronometer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private int miliseconds;
+
+        private bool isRunning;
+
+        public Chronometer()
+        {
+            this.Reset();
+        }
+
+        public string GetTime => $"{miliseconds / 60000:d2}:{miliseconds / 1000:d2}:{miliseconds % 1000}";
+
+        public List<string> Laps { get; private set; }
+
+        public void Lap()
+        {
+            Laps.Add(this.GetTime);
+        }
+
+        public void Reset()
+        {
+            this.Stop();
+            this.miliseconds = 0;
+            this.Laps = new List<string>();
+        }
+
+        public void Start()
+        {
+            this.isRunning = true;
+            Task.Run(() =>
+            {
+                while (isRunning)
+                {
+                    Thread.Sleep(1);
+                    this.miliseconds++;
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            this.isRunning = false;
+        }
+    }
+}
---
+++ Asynchronous processing/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Chronometer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IChronometer chronometer = new Chronometer();
+
+            var input = Console.ReadLine().ToLower();
+
+            while (input != "exit")
+            {
+                switch (input)
+                {
+                    case "start":
+                        chronometer.Start();
+                        break;
+                    case "stop":
+                        chronometer.Stop();
+                        break;
+                    case "lap":
+                        chronometer.Lap();
+                        break;
+                    case "time":
+                        Console.WriteLine(chronometer.GetTime);
+                        break;
+                    case "reset":
+                        chronometer.Reset();
+                        break;
+                }
+
+                input = Console.ReadLine().ToLower();
+            }
+        }
+    }
+}
